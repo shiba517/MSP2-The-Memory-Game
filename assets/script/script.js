0324045c98fe1c362b541f8a55dac6f0abd98a02
shiba517 <shiba.cdeb@gmail.com>
2021-06-07 15:54:39 +0000

    Section 2.1 - 2.3 are navigatable via buttons from section 1.1

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,7 +1,54 @@
 $(document).ready(function() {
+    // TARGETING TAGS VIA JQUERY
+    const footerTag = $('footer')
     const headerTag = $('header')
 
-    headerTag.click(function() {
-        console.log('I am working!')
+    // TARGETING ID TAGS VIA JQUERY
+    // const sectionGameMenu = $('#sectionGameMenu')
+    const sectionOpeningScreen = $('#sectionOpeningScreen')
+    const sectionHowToPlay = $('#sectionHowToPlay')
+    const sectionStartGame = $('#sectionStartGame')
+    const sectionAboutUs = $('#sectionAboutUs')
+    const sectionTheGame = $('#sectionTheGame')
+    const buttonHowToPlay = $('#button-howToPlay')
+    const buttonAboutUs = $('#button-aboutUs')
+    const buttonStartGame = $('#button-startGame')
+
+    // TARGETING CLASS VIA JQUERY
+    const cssDisplayNone = 'nbs-display-none'
+
+    // THIS ARRAY IS TO TARGET ALL THESE SECTIONS AT ONCE FOR SCREEN NAVIGATION
+    const allSection2s = [
+        {name: 'sectionOpeningScreen', jq: $('#sectionOpeningScreen')},
+        {name: 'sectionHowToPlay', jq: $('#sectionHowToPlay')},
+        {name: 'sectionStartGame', jq: $('#sectionStartGame')},
+        {name: 'sectionAboutUs', jq: $('#sectionAboutUs')},
+        {name: 'sectionTheGame', jq: $('#sectionTheGame')}
+    ]
+
+    // MAKES const allSections2s DISPLAY NONE
+    function makeSection2DisplayNone() {
+        for (let i = 0; i < allSection2s.length; i++) {
+            let currentSection = allSection2s[i].jq
+            currentSection.addClass(cssDisplayNone)
+        }
+    }
+
+    // DISPLAYS SECTION 2.2 - HOW TO PLAY SCREEN
+    buttonHowToPlay.click(function() {
+        makeSection2DisplayNone()
+        sectionHowToPlay.removeClass(cssDisplayNone)
+    })
+
+    // DISPLAYS SECTION 2.3 - ABOUT US SCREEN
+    buttonAboutUs.click(function() {
+        makeSection2DisplayNone()
+        sectionAboutUs.removeClass(cssDisplayNone)
+    })
+
+    // DISPLAYS SECTION 2.4 - START GAME SCREEN
+    buttonStartGame.click(function() {
+        makeSection2DisplayNone()
+        sectionStartGame.removeClass(cssDisplayNone)
     })
 })
